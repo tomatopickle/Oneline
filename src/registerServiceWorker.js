31c32a4a7c93da990c9718a2a7616b702249f0c4
tomatopickle <65482369+tomatopickle@users.noreply.github.com>
2022-05-20 10:13:55 +0000

    Feat: Add update downloaded notification

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
-
+let newContentAvailable = false;
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
@@ -18,6 +18,7 @@ if (process.env.NODE_ENV === 'production') {
     },
     updatefound() {
       console.log('New content is downloading.')
+      newContentAvailable = true;
     },
     updated() {
       console.log('New content is available; please refresh.');
@@ -25,6 +26,9 @@ if (process.env.NODE_ENV === 'production') {
         for (let name of names) caches.delete(name);
       });
       localStorage.setItem("lastUpdateTime", Date.now());
+      if(newContentAvailable){
+        notify(`New update downloaded, please refresh.`,'success','arrow-clockwise');
+      }
     },
     offline() {
       console.log('No internet connection found. App is running in offline mode.')
@@ -34,3 +38,24 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+ function escapeHtml(html) {
+    const div = document.createElement('div');
+    div.textContent = html;
+    return div.innerHTML;
+  }
+
+  // Custom function to emit toast notifications
+  function notify(message, variant = 'primary', icon = 'info-circle', duration = 3000) {
+    const alert = Object.assign(document.createElement('sl-alert'), {
+      variant,
+      closable: true,
+      duration: duration,
+      innerHTML: `
+        <sl-icon name="${icon}" slot="icon"></sl-icon>
+        ${escapeHtml(message)}
+      `
+    });
+
+    document.body.append(alert);
+    return alert.toast();
+  }
